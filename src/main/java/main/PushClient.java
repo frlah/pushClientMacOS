20f3fb0dae49fe2f31e0cb98ed5b8851547903e4
Fred Lahde <fredlahde@gmail.com>
2016-10-19 23:15:25 +0200

    Implemented Mac Screencapture. Removed JavaFX Junk

--- src/main/java/main/PushClient.java
+++ src/main/java/main/PushClient.java
@@ -2,22 +2,24 @@ package main;
 
 
 import config.Config;
-import image.ScreenGrab;
-import javafx.application.Application;
-import javafx.stage.Stage;
+import http.Upload;
 
-public class PushClient extends Application {
+import java.io.File;
+import java.io.IOException;
+
+public class PushClient {
 
     public static void main(String[] args) {
-        launch(args);
+        try {
+            final String path = "file.png";
+            final Process process = Runtime.getRuntime().exec("/usr/sbin/screencapture -i " + path);
+            process.waitFor();
+            final Config config = new Config();
+            Upload.uploadTempContent(new File(path), config.getProperties().getProperty("url"), config);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        PushClient pushClient = new PushClient();
-        Config config = new Config();
-        ScreenGrab screenGrab = new ScreenGrab(config, primaryStage);
 
-        screenGrab.start();
-    }
 }
